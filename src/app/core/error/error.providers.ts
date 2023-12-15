7e33be4bec72d20159852073cb7304dfbc72fbdb
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-12-15 22:49:20 +0100

    Add router binding for error routes

--- src/app/core/error/error.providers.ts
+++ src/app/core/error/error.providers.ts
@@ -1,6 +1,6 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { EnvironmentProviders, Provider } from '@angular/core';
-import { PreloadAllModules, withPreloading } from '@angular/router';
+import { PreloadAllModules, withComponentInputBinding, withPreloading } from '@angular/router';
 import { provideRootRouter } from '../../common/router-features';
 import { ERROR_ROUTES } from './error.routes';
 import { serverErrorInterceptor } from './interceptors/server-error.interceptor';
@@ -11,5 +11,6 @@ export const ERROR_PROVIDERS: (Provider | EnvironmentProviders)[] = [
   ])),
   provideRootRouter(ERROR_ROUTES,
     withPreloading(PreloadAllModules),
+    withComponentInputBinding(),
   ),
 ];
