36f9d4c98283fc430f0bdaf70ecc6d44566b6ae6
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-07 00:00:23 +0100

    Fix ChargeWidgetComponent tests

--- src/app/pages/home/groups/charge/components/charge-widget/charge-widget.component.spec.ts
+++ src/app/pages/home/groups/charge/components/charge-widget/charge-widget.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRenaultClient } from '@remscodes/ngx-renault-api-client';
-import { mockAccountId } from '../../../../../../../tests/mocks/mock.constants';
+import { mockAccountId, mockVin } from '../../../../../../../tests/mocks/mock.constants';
 import { AuthStore } from '../../../../../../core/auth/services/auth-store.service';
+import { VehicleStore } from '../../../../../../core/renault/services/vehicle-store.service';
 import { ChargeWidgetComponent } from './charge-widget.component';
 
 describe('ChargeWidgetComponent', () => {
   let fixture: ComponentFixture<ChargeWidgetComponent>;
   let component: ChargeWidgetComponent;
   let authStore: AuthStore;
+  let vehicleStore: VehicleStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,8 +22,10 @@ describe('ChargeWidgetComponent', () => {
     fixture = TestBed.createComponent(ChargeWidgetComponent);
     component = fixture.componentInstance;
     authStore = fixture.debugElement.injector.get(AuthStore);
+    vehicleStore = fixture.debugElement.injector.get(VehicleStore);
 
     authStore.accountId.set(mockAccountId);
+    vehicleStore.vin.set(mockVin);
 
     fixture.detectChanges();
   });
